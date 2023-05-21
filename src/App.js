553ed2de2266462a7de77c0542f84a932ca9d6c9
darioaladuz <baladuz98@gmail.com>
2023-05-21 13:34:55 +0200

    Update, fix bugs, dependencies...

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-import useFetch from './useFetch';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 
 function App() {
-
-  let cities = useFetch('https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json');
-
   const [render, setRender] = useState({name: '', code: '', lat: '', lng: ''});
 
-  let Icon = () => {
-    return (
-    <div>
-      <p>xxx</p>
-    </div>
-    )
-  }
- 
-  setTimeout(() => {
-    console.log(cities[0])
-  }, 1500)
   function deg_to_dms (deg, dir) {
     if (deg < 0) {
       deg = deg * -1;
-      dir = dir == 'N' ? 'S' : 'W';
+      dir = dir === 'N' ? 'S' : 'W';
     }
     var d = Math.floor (deg);
     var minfloat = (deg-d)*60;
@@ -32,32 +18,33 @@ function App() {
     var s = Math.round(secfloat);
     // After rounding, the seconds might become 60. These two
     // if-tests are not necessary if no rounding is done.
-    if (s==60) {
+    if (s === 60) {
       m++;
       s=0;
     }
-    if (m==60) {
+    if (m === 60) {
       d++;
       m=0;
     }
     return (`${d}º${m}'${s}"${dir}`);
  }
 
-  function clickMe() {
-    let city = cities[0][Math.floor(Math.random() * cities[0].length)];
+  async function clickMe() {
+    let cities = await fetch('https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json');
+    let citiesJSON = await cities.json();
+
+    let city = await citiesJSON[Math.floor(Math.random() * citiesJSON.length)];
+    console.log(city);
     setRender({name: city.name, code: city.country, lat: deg_to_dms(city.lat, 'N'), lng: deg_to_dms(city.lng, 'E')})
-
-    console.log(render);
-
   }
 
   return (
     <div className="App">
       <h1>Travel <span>roulette</span><i className="fas fa-route"></i></h1>
-      <p>Dale al botón para generar aleatoriamente una de las <span>128.769</span> ciudades disponibles en nuestra base de datos y un enlace al mapa de la ciudad generada. Bon voyage!</p>
-      <button onClick={clickMe}>Generar</button>
-      <div className="go">Vámonos a... <span class='hl'>{render.name}, {render.code}</span></div>
-      <div><a href={`https://www.google.com/maps/place/${render.lat}+${render.lng}`} target="_blank">{render.name !== '' ? `Click para ver el mapa` : ''}</a></div>
+      <p>Click the button to retrieve one of the <span>128.769</span> cities available in our database and the Google Maps url for the city. Bon voyage!</p>
+      <button onClick={clickMe}>Generate</button>
+      <div className="go">Let's go to... { render.name && <><span className='hl'>{render.name}, {render.code}</span> <span class={`fi fi-${render.code.toLowerCase()}`}></span> </>}</div>
+      <div><a href={`https://www.google.com/maps/place/${render.lat}+${render.lng}`} target="_blank">{render.name !== '' ? `Click to see the map` : ''}</a></div>
       <h6 className="ft">Dario Aladuz</h6>
     </div>
   );
--- src/useFetch.js
+++
@@ -1,9 +0,0 @@
-export default function useFetch(endpoint) {
-    let cities = [];
-    
-    fetch(endpoint)
-        .then(blob => blob.json())
-        .then(data => cities.push(data))
-    
-    return cities;
-}
